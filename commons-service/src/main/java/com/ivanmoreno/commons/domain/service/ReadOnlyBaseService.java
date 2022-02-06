796d0b29ffdee9118eecb1bbb303796e936df51c
ivan <ivanmoreno405@gmail.com>
2022-02-05 20:05:27 -0600

    Services were moved from restaurant service to here [commons]

---
+++ commons-service/src/main/java/com/ivanmoreno/commons/domain/service/ReadOnlyBaseService.java
@@ -0,0 +1,12 @@
+package com.ivanmoreno.commons.domain.service;
+
+import com.ivanmoreno.commons.domain.repository.ReadOnlyRepository;
+
+public abstract class ReadOnlyBaseService<TE, T> {
+
+	private ReadOnlyRepository<TE, T> repository;
+	
+	public ReadOnlyBaseService(ReadOnlyRepository<TE, T>  repo) {
+		this.repository = repo;
+	}
+}
